8f881a0d1c22b3a67af3a243f7bba0fd5ce45a26
Pushti1208 <pushtiharsola1208@gmail.com>
2024-01-25 17:11:45 +0530

    messed message

--- first.cpp
+++ first.cpp
@@ -3,16 +3,5 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-
-    if(n==1)
-    {
-        cout<<"hello git";
-    }
-    else{
-        cout<<"bye git";
-        cout<<"leave git";
-        cout<<"go from here";
-    }
+    cout<<"Messed";
 }
